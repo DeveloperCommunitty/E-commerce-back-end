edb9b30fefb5c4594ba328d75afdf89c09f86bd1
PHziinn <skamikaze4@gmail.com>
2024-09-30 22:10:04 -0300

    chore: adicionando PrismaService.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { PrismaService } from './database/PrismaService';
 
 @Module({
   imports: [UsuarioModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
