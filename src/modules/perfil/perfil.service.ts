8294b5394be6082e0e33a33a446d3dc8ea1fbbc2
PHziinn <skamikaze4@gmail.com>
2024-10-06 22:46:32 -0300

    chore(remove): removendo comentarios

--- src/modules/perfil/perfil.service.ts
+++ src/modules/perfil/perfil.service.ts
@@ -5,77 +5,97 @@ import { PrismaService } from 'src/database/PrismaService';
 
 @Injectable()
 export class ProfileService {
-
-  constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
   async create(@Body() body: CreateProfileDto) {
-    const { firstName, lastName, cpf, birthDate, ddd, phone, genero, userId } = body;
+    const { firstName, lastName, cpf, birthDate, ddd, phone, genero, userId } =
+      body;
 
-    const userCheck = await this.prisma.user.findFirst({where: {id: userId}})
-    if(!userCheck) throw new HttpException(`Usuário inexistente`, HttpStatus.NOT_FOUND)
-    
-    const cpfCheck = await this.prisma.profiles.findFirst({where: {cpf}})
-    if(cpfCheck) throw new HttpException(`Cpf já está sendo usado`, HttpStatus.BAD_REQUEST)
-    
+    const userCheck = await this.prisma.user.findFirst({
+      where: { id: userId },
+    });
+    if (!userCheck)
+      throw new HttpException(`Usuário inexistente`, HttpStatus.NOT_FOUND);
+
+    const cpfCheck = await this.prisma.profiles.findFirst({ where: { cpf } });
+    if (cpfCheck)
+      throw new HttpException(
+        `Cpf já está sendo usado`,
+        HttpStatus.BAD_REQUEST,
+      );
+
     const profileCreate = await this.prisma.profiles.create({
       data: {
-        firstName, 
-        lastName, 
-        cpf, 
-        birthDate, 
-        ddd, 
-        phone, 
+        firstName,
+        lastName,
+        cpf,
+        birthDate,
+        ddd,
+        phone,
         genero,
-        userId
-      }
-    })
+        userId,
+      },
+    });
 
-    if(!profileCreate) throw new HttpException(`Erro ao criar perfil`, HttpStatus.EXPECTATION_FAILED)
+    if (!profileCreate)
+      throw new HttpException(
+        `Erro ao criar perfil`,
+        HttpStatus.EXPECTATION_FAILED,
+      );
 
-    return profileCreate
+    return profileCreate;
   }
 
   async findAll() {
     const allProfiles = await this.prisma.profiles.findMany({
       orderBy: {
-        id: 'desc'
-      }, 
+        id: 'desc',
+      },
       select: {
-        id: true, 
-        firstName: true, 
-        lastName: true, 
-        cpf: true, 
-        birthDate: true, 
-        phone: true, 
+        id: true,
+        firstName: true,
+        lastName: true,
+        cpf: true,
+        birthDate: true,
+        phone: true,
         genero: true,
-        userId: true
-      }
-    })
+        userId: true,
+      },
+    });
 
-    if(!allProfiles) throw new HttpException(`Erro ao listar perfis`, HttpStatus.EXPECTATION_FAILED)
-  
-    return allProfiles
-}
+    if (!allProfiles)
+      throw new HttpException(
+        `Erro ao listar perfis`,
+        HttpStatus.EXPECTATION_FAILED,
+      );
+
+    return allProfiles;
+  }
 
   async findOne(id: string) {
-    const profile = await this.prisma.profiles.findUnique({where: {id}})
-    
-    if(!profile) throw new HttpException(`Perfil inexistente`, HttpStatus.NOT_FOUND)
+    const profile = await this.prisma.profiles.findUnique({ where: { id } });
 
-    return profile
+    if (!profile)
+      throw new HttpException(`Perfil inexistente`, HttpStatus.NOT_FOUND);
+
+    return profile;
   }
 
   async update(id: string, body: UpdateProfileDto) {
-    const profileCheck = await this.prisma.profiles.findFirst({where: {id}})
-    if(!profileCheck) throw new HttpException(`Perfil inexistente`, HttpStatus.NOT_FOUND)
+    const profileCheck = await this.prisma.profiles.findFirst({
+      where: { id },
+    });
+    if (!profileCheck)
+      throw new HttpException(`Perfil inexistente`, HttpStatus.NOT_FOUND);
 
-    const {firstName, lastName, cpf, birthDate, ddd, phone, genero, userId } = body
+    const { firstName, lastName, cpf, birthDate, ddd, phone, genero, userId } =
+      body;
 
-    const userCheck = await this.prisma.user.findFirst({where: {id: userId}})
-    if(!userCheck) throw new HttpException(`Usuário inexistente`, HttpStatus.NOT_FOUND)
-
-    // const cpfCheck = await this.prisma.profiles.findUnique({where: {cpf}})
-    // if(cpfCheck) throw new HttpException(`CPF já está em uso`, HttpStatus.BAD_REQUEST)
+    const userCheck = await this.prisma.user.findFirst({
+      where: { id: userId },
+    });
+    if (!userCheck)
+      throw new HttpException(`Usuário inexistente`, HttpStatus.NOT_FOUND);
 
     const profile = await this.prisma.profiles.update({
       where: {
@@ -88,23 +108,37 @@ export class ProfileService {
         birthDate,
         ddd,
         phone,
-        genero
+        genero,
       },
     });
 
-    if(!profile) throw new HttpException(`Erro ao atualizar perfil`, HttpStatus.EXPECTATION_FAILED)
-    
+    if (!profile)
+      throw new HttpException(
+        `Erro ao atualizar perfil`,
+        HttpStatus.EXPECTATION_FAILED,
+      );
+
     return profile;
   }
 
   async remove(id: string) {
-    const profileCheck = await this.prisma.profiles.findUnique({where: {id}})
-    if(!profileCheck) throw new HttpException(`Perfil inexistente`, HttpStatus.NOT_FOUND)
+    const profileCheck = await this.prisma.profiles.findUnique({
+      where: { id },
+    });
+    if (!profileCheck)
+      throw new HttpException(`Perfil inexistente`, HttpStatus.NOT_FOUND);
 
-    const profileDeleted = await this.prisma.profiles.delete({where: {id}})
+    const profileDeleted = await this.prisma.profiles.delete({ where: { id } });
 
-    if(!profileDeleted) throw new HttpException(`Erro ao deletar perfil`, HttpStatus.EXPECTATION_FAILED)
+    if (!profileDeleted)
+      throw new HttpException(
+        `Erro ao deletar perfil`,
+        HttpStatus.EXPECTATION_FAILED,
+      );
 
-    throw new HttpException(`Perfil deletado com sucesso`, HttpStatus.NO_CONTENT)
+    throw new HttpException(
+      `Perfil deletado com sucesso`,
+      HttpStatus.NO_CONTENT,
+    );
   }
 }
