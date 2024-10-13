ea3f997ff4c560bea1606eae8b10f4364b74ee47
PHziinn <skamikaze4@gmail.com>
2024-10-13 15:34:29 -0300

    chore(refactor): Refatoração do código

--- src/modules/produtos/produtos.controller.ts
+++ src/modules/produtos/produtos.controller.ts
@@ -1,9 +1,25 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProdutosService } from './produtos.service';
 import { CheckPolicies } from 'src/casl/guards/policies.check';
 import { AppAbility } from 'src/casl/casl-ability.factory/casl-ability.factory';
 import { Action } from 'src/casl/casl-ability.factory/actionDto/casl-action.dto';
-import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ProductsDto } from './dto/produtos.dto';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { CloudinaryStorageConfig } from 'src/cloudinary/Multer.config';
@@ -11,94 +27,103 @@ import { CloudinaryStorageConfig } from 'src/cloudinary/Multer.config';
 @ApiTags('Produto')
 @Controller('produto')
 export class ProdutosController {
-    constructor(private readonly productsService: ProdutosService){}
+  constructor(private readonly productsService: ProdutosService) {}
 
-    @ApiOperation({ summary: 'Lista todos os produtos.' })
-    @ApiBearerAuth()
-    @ApiResponse({ status: 204})  
-    @ApiResponse({ status: 404, description: 'Erro ao listar produtos' })
-    @Get('produtos')
-    findAll(){
-        return this.productsService.findAll();
-    }
+  @ApiOperation({ summary: 'Lista todos os produtos.' })
+  @ApiBearerAuth()
+  @ApiResponse({ status: 204 })
+  @ApiResponse({ status: 404, description: 'Erro ao listar produtos' })
+  @Get('produtos')
+  findAll() {
+    return this.productsService.findAll();
+  }
 
-    @Post()
-    @UseInterceptors(
-      FilesInterceptor('files', 10, { 
-        storage: CloudinaryStorageConfig,
-        fileFilter: (req, file, cb) => {
-          const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg'];
-          if (!allowedTypes.includes(file.mimetype)) {
-            return cb(new Error('Tipo de arquivo inválido'), false);
-          }
-          cb(null, true);
-        },
-      }),
-    )
-    @ApiOperation({
-        summary: 'Cria produto',
-        description: 'Disponível apenas para Admin. Adicione a imagem com a chave: files.',
-      })
-    @ApiResponse({ status: 204})  
-    @ApiResponse({ status: 417, description: 'O produto já existe!' })  
-    @ApiResponse({ status: 417, description: 'Erro ao criar produto' })  
-    @ApiBearerAuth()
-    @CheckPolicies((ability: AppAbility) => ability.can(Action.Admin, 'all'))
-    create(@Body() body: ProductsDto, @UploadedFiles() files: Express.Multer.File[]){
-        return this.productsService.create(body, files);
-    }
+  @Post()
+  @UseInterceptors(
+    FilesInterceptor('files', 10, {
+      storage: CloudinaryStorageConfig,
+      fileFilter: (req, file, cb) => {
+        const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+        if (!allowedTypes.includes(file.mimetype)) {
+          return cb(new Error('Tipo de arquivo inválido'), false);
+        }
+        cb(null, true);
+      },
+    }),
+  )
+  @ApiOperation({
+    summary: 'Cria produto',
+    description:
+      'Disponível apenas para Admin. Adicione a imagem com a chave: files.',
+  })
+  @ApiResponse({ status: 204 })
+  @ApiResponse({ status: 417, description: 'O produto já existe!' })
+  @ApiResponse({ status: 417, description: 'Erro ao criar produto' })
+  @ApiBearerAuth()
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Admin, 'all'))
+  create(
+    @Body() body: ProductsDto,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    return this.productsService.create(body, files);
+  }
 
+  @UseInterceptors(
+    FilesInterceptor('files', 10, {
+      storage: CloudinaryStorageConfig,
+      fileFilter: (req, file, cb) => {
+        const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+        if (!allowedTypes.includes(file.mimetype)) {
+          return cb(new Error('Tipo de arquivo inválido'), false);
+        }
+        cb(null, true);
+      },
+    }),
+  )
+  @ApiOperation({
+    summary: 'Atualiza dados do produto',
+    description:
+      'Disponível apenas para Admin. Atualização da imagem é opcional. Adicione a imagem com a chave: files.',
+  })
+  @ApiParam({ name: 'id', description: 'Id do produto' })
+  @ApiResponse({ status: 404, description: 'Produto inexistente!' })
+  @ApiResponse({
+    status: 417,
+    description: 'Nome do produto já está cadastrado!',
+  })
+  @ApiResponse({ status: 417, description: 'Erro ao atualizar produto' })
+  @ApiBearerAuth()
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Admin, 'all'))
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() body: ProductsDto,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    return this.productsService.update(id, body, files);
+  }
 
-    @UseInterceptors(
-        FilesInterceptor('files', 10, { 
-          storage: CloudinaryStorageConfig,
-          fileFilter: (req, file, cb) => {
-            const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg'];
-            if (!allowedTypes.includes(file.mimetype)) {
-              return cb(new Error('Tipo de arquivo inválido'), false);
-            }
-            cb(null, true);
-            },
-        }),
-    )
-    @ApiOperation({
-        summary: 'Atualiza dados do produto' ,
-        description: 'Disponível apenas para Admin. Atualização da imagem é opcional. Adicione a imagem com a chave: files.',
-      })
-    @ApiParam({ name: 'id', description: 'Id do produto'})
-    @ApiResponse({ status: 404, description: 'Produto inexistente!' })  
-    @ApiResponse({ status: 417, description: 'Nome do produto já está cadastrado!' })  
-    @ApiResponse({ status: 417, description: 'Erro ao atualizar produto' })  
-    @ApiBearerAuth()
-    @CheckPolicies((ability: AppAbility) => ability.can(Action.Admin, 'all'))
-    @Put(':id')
-    update(@Param('id') id: string, @Body() body: ProductsDto, @UploadedFiles() files: Express.Multer.File[]){
-        return this.productsService.update(id, body, files);
-    }
+  @ApiOperation({ summary: 'Lista produto por Id' })
+  @ApiBearerAuth()
+  @ApiParam({ name: 'id', description: 'Id do produto' })
+  @ApiResponse({ status: 404, description: 'Produto inexistente!' })
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(id);
+  }
 
-    @ApiOperation({ summary: 'Lista produto por Id' }) 
-    @ApiBearerAuth()
-    @ApiParam({ name: 'id', description: 'Id do produto'}) 
-    @ApiResponse({ status: 404, description: 'Produto inexistente!' })  
-    @Get(':id')
-    findOne(@Param('id') id: string){
-        return this.productsService.findOne(id);
-    }
-
-
-    @ApiOperation({
-        summary: 'Deleta produto por Id'  ,
-        description: 'Disponível apenas para Admin.',
-      })
-    @ApiParam({ name: 'id', description: 'Id do produto'}) 
-    @ApiResponse({ status: 204})  
-    @ApiResponse({ status: 404, description: 'Produto inexistente!' })  
-    @ApiResponse({ status: 417, description: 'Erro ao atualizar produto' })  
-    @Delete(':id')
-    @ApiBearerAuth()
-    @CheckPolicies((ability: AppAbility) => ability.can(Action.Admin, 'all'))
-    destroy(@Param('id') id: string){
-        return this.productsService.destroy(id);
-    }
-
+  @ApiOperation({
+    summary: 'Deleta produto por Id',
+    description: 'Disponível apenas para Admin.',
+  })
+  @ApiParam({ name: 'id', description: 'Id do produto' })
+  @ApiResponse({ status: 204 })
+  @ApiResponse({ status: 404, description: 'Produto inexistente!' })
+  @ApiResponse({ status: 417, description: 'Erro ao atualizar produto' })
+  @Delete(':id')
+  @ApiBearerAuth()
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Admin, 'all'))
+  destroy(@Param('id') id: string) {
+    return this.productsService.destroy(id);
+  }
 }
--- src/modules/produtos/produtos.service.ts
+++ src/modules/produtos/produtos.service.ts
@@ -1,206 +1,264 @@
-import { Body, HttpException, HttpStatus, Injectable, UploadedFiles } from '@nestjs/common';
+import {
+  Body,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UploadedFiles,
+} from '@nestjs/common';
 import { PrismaService } from 'src/database/PrismaService';
 import { ProductsDto } from './dto/produtos.dto';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Injectable()
 export class ProdutosService {
-    constructor(
-        private readonly prisma: PrismaService,
-        private readonly cloudinary: CloudinaryService
-    ) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly cloudinary: CloudinaryService,
+  ) {}
 
-    async create(@Body() body: ProductsDto, @UploadedFiles() files: Express.Multer.File[]){
-        const { name, description, price, stock, sku} = body;
+  async create(
+    @Body() body: ProductsDto,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    const { name, description, price, stock, sku } = body;
 
-        const nameUnique = await this.prisma.products.findFirst({
-            where:{
-                name: name
-            },
-        });
+    const nameUnique = await this.prisma.products.findFirst({
+      where: {
+        name: name,
+      },
+    });
 
-        if (nameUnique) throw new HttpException(`O produto já existe!`, HttpStatus.EXPECTATION_FAILED);
+    if (nameUnique)
+      throw new HttpException(
+        `O produto já existe!`,
+        HttpStatus.EXPECTATION_FAILED,
+      );
 
-        const skuUnique = await this.prisma.products.findFirst({
-            where:{
-                sku: sku
-            },
-        });
+    const skuUnique = await this.prisma.products.findFirst({
+      where: {
+        sku: sku,
+      },
+    });
 
-        if (skuUnique) throw new HttpException(`O código do produto já existe!`, HttpStatus.EXPECTATION_FAILED);
+    if (skuUnique)
+      throw new HttpException(
+        `O código do produto já existe!`,
+        HttpStatus.EXPECTATION_FAILED,
+      );
 
-
-        let imageResults 
-        if (files && files.length > 0 && !nameUnique && !skuUnique) {
-            imageResults = await Promise.all(files.map(file => this.cloudinary.uploadImage(file)));
-        } else {
-            throw new HttpException('Nenhum arquivo enviado ou produto existente.', HttpStatus.NOT_FOUND);
-        } 
-
-        const product = await this.prisma.products.create({
-            data: {
-                name, 
-                description, 
-                price, 
-                stock, 
-                sku, 
-                imagemUrl: imageResults.map(element => element.url).join(', '),
-                publicId:  imageResults.map(element => element.public_id).join(', '),
-            },
-            select: {
-                id: true,
-                name: true,
-                price: true,
-                description: true,
-                sku: true,
-                stock: true,
-                imagemUrl: true,
-                publicId: true,
-                CartItems: true,
-                createdAt: true,
-            }
-        });
-
-        if (!product) throw new HttpException(`Erro ao criar produto`, HttpStatus.EXPECTATION_FAILED);
-
-        return product;
+    let imageResults;
+    if (files && files.length > 0 && !nameUnique && !skuUnique) {
+      imageResults = await Promise.all(
+        files.map((file) => this.cloudinary.uploadImage(file)),
+      );
+    } else {
+      throw new HttpException(
+        'Nenhum arquivo enviado ou produto existente.',
+        HttpStatus.NOT_FOUND,
+      );
     }
 
-    async findAll(){
-        const productsAll = await this.prisma.products.findMany({
-            select:{
-                id: true,
-                name: true,
-                price: true,
-                description: true,
-                sku: true,
-                stock: true,
-                imagemUrl: true,
-                publicId: true,
-                CartItems: true,
-                createdAt: true,
-            }
-        });
+    const product = await this.prisma.products.create({
+      data: {
+        name,
+        description,
+        price,
+        stock,
+        sku,
+        imagemUrl: imageResults.map((element) => element.url).join(', '),
+        publicId: imageResults.map((element) => element.public_id).join(', '),
+      },
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        description: true,
+        sku: true,
+        stock: true,
+        imagemUrl: true,
+        publicId: true,
+        CartItems: true,
+        createdAt: true,
+      },
+    });
 
-        if (!productsAll) throw new HttpException(`Erro ao listar produtos`, HttpStatus.EXPECTATION_FAILED,);
+    if (!product)
+      throw new HttpException(
+        `Erro ao criar produto`,
+        HttpStatus.EXPECTATION_FAILED,
+      );
 
-        return productsAll;
-    }
+    return product;
+  }
 
-    async update(@Body() id: string, body: ProductsDto, @UploadedFiles() files: Express.Multer.File[]){
-        const { name, description, price, stock, sku } = body;
+  async findAll() {
+    const productsAll = await this.prisma.products.findMany({
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        description: true,
+        sku: true,
+        stock: true,
+        imagemUrl: true,
+        publicId: true,
+        CartItems: true,
+        createdAt: true,
+      },
+    });
 
-        const existsProduct = await this.prisma.products.findUnique({where: {id}});
+    if (!productsAll)
+      throw new HttpException(
+        `Erro ao listar produtos`,
+        HttpStatus.EXPECTATION_FAILED,
+      );
 
-        if(!existsProduct) throw new HttpException(`Produto inexistente!`, HttpStatus.NOT_FOUND);
+    return productsAll;
+  }
 
-        const nameUnique = await this.prisma.products.findFirst({
-            where: {
-                sku: sku,
-                id: { not: id }
-            },
-        });
+  async update(
+    @Body() id: string,
+    body: ProductsDto,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    const { name, description, price, stock, sku } = body;
 
-        if (nameUnique) throw new HttpException(`Nome do produto já está cadastrado!`, HttpStatus.EXPECTATION_FAILED);
+    const existsProduct = await this.prisma.products.findUnique({
+      where: { id },
+    });
 
-        const skuUnique = await this.prisma.products.findFirst({
-            where: {
-                sku: sku,
-                id: { not: id } 
-            },
-        });
+    if (!existsProduct)
+      throw new HttpException(`Produto inexistente!`, HttpStatus.NOT_FOUND);
 
-        if (skuUnique) throw new HttpException(`O código do produto já existe!`, HttpStatus.EXPECTATION_FAILED);
+    const nameUnique = await this.prisma.products.findFirst({
+      where: {
+        sku: sku,
+        id: { not: id },
+      },
+    });
 
+    if (nameUnique)
+      throw new HttpException(
+        `Nome do produto já está cadastrado!`,
+        HttpStatus.EXPECTATION_FAILED,
+      );
 
-        if (existsProduct.publicId && files &&files.length > 0) {
-            const publicIds = existsProduct.publicId.split(', '); 
-          
-            await Promise.all(publicIds.map(async (publicId) => {
-                try {
-                    await this.cloudinary.destroy(publicId);
-                    console.log(`Imagem ${publicId} apagada com sucesso.`);
-                } catch (error) {
-                    throw new HttpException(`Erro ao apagar a imagem ${publicId}: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
-                }
-            }));
+    const skuUnique = await this.prisma.products.findFirst({
+      where: {
+        sku: sku,
+        id: { not: id },
+      },
+    });
 
-            let imageResults 
-            imageResults = await Promise.all(files.map(file => this.cloudinary.uploadImage(file)));
-            const product = await this.prisma.products.update({
-                where:{
-                    id,
-                },
-                data:{
-                    name, 
-                    description, 
-                    price, 
-                    stock, 
-                    sku, 
-                    imagemUrl: imageResults.map(element => element.url).join(', '),
-                    publicId:  imageResults.map(element => element.public_id).join(', '),
-                }
-            });
-    
-            if (!product) throw new HttpException(`Erro ao atualizar produto`, HttpStatus.EXPECTATION_FAILED);
-    
-            return product;
-        }
-        else {
-            const product = await this.prisma.products.update({
-                where:{
-                    id,
-                },
-                data:{
-                    name, 
-                    description, 
-                    price, 
-                    stock, 
-                    sku,
-                }
-            });
-    
-            if (!product) throw new HttpException(`Erro ao atualizar produto`, HttpStatus.EXPECTATION_FAILED);
-    
-            return product;
-        } 
+    if (skuUnique)
+      throw new HttpException(
+        `O código do produto já existe!`,
+        HttpStatus.EXPECTATION_FAILED,
+      );
 
-    }
+    if (existsProduct.publicId && files && files.length > 0) {
+      const publicIds = existsProduct.publicId.split(', ');
 
-    async findOne(id: string){
-        const product = await this.prisma.products.findUnique({where: {id}})
-
-        if(!product) throw new HttpException(`Produto inexistente!`, HttpStatus.NOT_FOUND);
-
-        return product;
-    }
-
-    async destroy(id: string){
-        const product = await this.prisma.products.findUnique({where: {id}});
-
-        if(!product) throw new HttpException(`Produto inexistente!`, HttpStatus.NOT_FOUND);
-
-        if (product.publicId) {
-            const publicIds = product.publicId.split(', '); 
-          
-            await Promise.all(publicIds.map(async (publicId) => {
-              try {
-                await this.cloudinary.destroy(publicId);
-                console.log(`Imagem ${publicId} apagada com sucesso.`);
-              } catch (error) {
-                console.error(`Erro ao apagar a imagem ${publicId}:`, error);
-              }
-            }));
+      await Promise.all(
+        publicIds.map(async (publicId) => {
+          try {
+            await this.cloudinary.destroy(publicId);
+            console.log(`Imagem ${publicId} apagada com sucesso.`);
+          } catch (error) {
+            throw new HttpException(
+              `Erro ao apagar a imagem ${publicId}: ${error.message}`,
+              HttpStatus.INTERNAL_SERVER_ERROR,
+            );
           }
-        
-        await this.prisma.products.delete({
-            where: {id}
-        });
+        }),
+      );
 
+      let imageResults;
+      imageResults = await Promise.all(
+        files.map((file) => this.cloudinary.uploadImage(file)),
+      );
+      const product = await this.prisma.products.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+          description,
+          price,
+          stock,
+          sku,
+          imagemUrl: imageResults.map((element) => element.url).join(', '),
+          publicId: imageResults.map((element) => element.public_id).join(', '),
+        },
+      });
+
+      if (!product)
         throw new HttpException(
-            `Produto deletado com sucesso`,
-            HttpStatus.NO_CONTENT,
+          `Erro ao atualizar produto`,
+          HttpStatus.EXPECTATION_FAILED,
         );
+
+      return product;
+    } else {
+      const product = await this.prisma.products.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+          description,
+          price,
+          stock,
+          sku,
+        },
+      });
+
+      if (!product)
+        throw new HttpException(
+          `Erro ao atualizar produto`,
+          HttpStatus.EXPECTATION_FAILED,
+        );
+
+      return product;
+    }
+  }
+
+  async findOne(id: string) {
+    const product = await this.prisma.products.findUnique({ where: { id } });
+
+    if (!product)
+      throw new HttpException(`Produto inexistente!`, HttpStatus.NOT_FOUND);
+
+    return product;
+  }
+
+  async destroy(id: string) {
+    const product = await this.prisma.products.findUnique({ where: { id } });
+
+    if (!product)
+      throw new HttpException(`Produto inexistente!`, HttpStatus.NOT_FOUND);
+
+    if (product.publicId) {
+      const publicIds = product.publicId.split(', ');
+
+      await Promise.all(
+        publicIds.map(async (publicId) => {
+          try {
+            await this.cloudinary.destroy(publicId);
+          } catch (error) {
+            console.error(`Erro ao apagar a imagem ${publicId}:`, error);
+          }
+        }),
+      );
     }
 
+    await this.prisma.products.delete({
+      where: { id },
+    });
+
+    return {
+      message: 'Produto deletado com sucesso',
+      status: HttpStatus.NO_CONTENT,
+    };
+  }
 }
