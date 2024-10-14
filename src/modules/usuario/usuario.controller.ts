1cefed7c8099fee2c18e983886fa52823dd6c2bd
PHziinn <skamikaze4@gmail.com>
2024-10-13 23:31:25 -0300

    remove: Removendo findOneForEmail

--- src/modules/usuario/usuario.controller.ts
+++ src/modules/usuario/usuario.controller.ts
@@ -6,24 +6,28 @@ import {
   Param,
   Patch,
   UploadedFile,
-  UploadedFiles,
-  UseInterceptors
+  UseInterceptors,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CloudinaryStorageConfig } from 'src/cloudinary/Multer.config';
 import { UpdateUsuarioDto } from './dto/updateUsuario.dto';
 import { UsuarioService } from './usuario.service';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
-import { CloudinaryStorageConfig } from 'src/cloudinary/Multer.config';
 
 @ApiTags('Usuarios')
 @Controller('usuario')
 export class UsuarioController {
-  constructor(private usuario: UsuarioService) { }
+  constructor(private usuario: UsuarioService) {}
 
   @Get('usuarios')
   @ApiOperation({ summary: 'Lista todos os usuários' })
   @ApiResponse({ status: 200 })
-  @ApiResponse({ status: 417, description: `Nenhum usuário encontrado`})
+  @ApiResponse({ status: 417, description: `Nenhum usuário encontrado` })
   @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
   @ApiBearerAuth()
   findAll() {
@@ -33,26 +37,16 @@ export class UsuarioController {
   @Get(':id')
   @ApiOperation({ summary: 'Lista um usuário por id' })
   @ApiResponse({ status: 200 })
-  @ApiResponse({ status: 404, description: `Usuário não encontrado`})
+  @ApiResponse({ status: 404, description: `Usuário não encontrado` })
   @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
   @ApiBearerAuth()
   findOne(@Param('id') id: string) {
     return this.usuario.findOne(id);
   }
 
-  @Get('email/:email')
-  @ApiOperation({ summary: 'Lista um usuário por email' })
-  @ApiResponse({ status: 200 })
-  @ApiResponse({ status: 404, description: `Não foi possível localizar o Usuário`})
-  @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
-  @ApiBearerAuth()
-  findOneForEmail(@Param('email') email: string) {
-    return this.usuario.findOneForEmail(email);
-  }
-
   @Patch(':id')
   @UseInterceptors(
-    FileInterceptor('file', { 
+    FileInterceptor('file', {
       storage: CloudinaryStorageConfig,
       fileFilter: (req, file, cb) => {
         const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg'];
@@ -65,17 +59,21 @@ export class UsuarioController {
   )
   @ApiOperation({ summary: 'Atualiza um usuário por id' })
   @ApiResponse({ status: 200, description: `Usuário atualizado com sucesso` })
-  @ApiResponse({ status: 404, description: `Usuário inexistente`})
+  @ApiResponse({ status: 404, description: `Usuário inexistente` })
   @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
   @ApiBearerAuth()
-  update(@Param('id') id: string, @Body() body: UpdateUsuarioDto, @UploadedFile() file: Express.Multer.File) {
+  update(
+    @Param('id') id: string,
+    @Body() body: UpdateUsuarioDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
     return this.usuario.update(id, body, file);
   }
 
   @Delete(':id')
   @ApiOperation({ summary: 'Deleta um usuário por id' })
   @ApiResponse({ status: 204, description: `Usuário deletado com sucesso` })
-  @ApiResponse({ status: 404, description: `Usuário inexistente`})
+  @ApiResponse({ status: 404, description: `Usuário inexistente` })
   @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
   @ApiBearerAuth()
   remove(@Param('id') id: string) {
