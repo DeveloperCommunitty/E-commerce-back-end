273ba3b8c19a31f56c4b63ac302a88aa52400f56
PHziinn <skamikaze4@gmail.com>
2024-10-18 20:19:27 -0300

    ⚡️ enhancement: Melhoria geral em carrinho e usuario

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -1,34 +1,48 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CreateCartDTO } from './dto/cart.create.dto';
 import { CheckPolicies } from 'src/casl/guards/policies.check';
 import { AppAbility } from 'src/casl/casl-ability.factory/casl-ability.factory';
 import { Action } from 'src/casl/casl-ability.factory/actionDto/casl-action.dto';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('Carrinho')
-@Controller('carrinho')
+@Controller('carrinhos')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
   @Post()
-  @ApiOperation({summary: 'Cria o carrinho'})
+  @ApiOperation({ summary: 'Cria o carrinho' })
   @ApiBody({ type: CreateCartDTO })
   @ApiResponse({ status: 200, description: 'Listagem gerada com sucesso' })
   @ApiResponse({ status: 400, description: 'Erro ao criar carrinho' })
   @ApiResponse({ status: 401, description: 'Usuário não autorizado' })
-  @ApiResponse({ status: 404, description: 'Produto não encontrado'})
+  @ApiResponse({ status: 404, description: 'Produto não encontrado' })
   @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
   @ApiBearerAuth('access_token')
   @CheckPolicies((ability: AppAbility) => ability.can(Action.User, 'all'))
-  async create(@Body() body: CreateCartDTO){
+  async create(@Body() body: CreateCartDTO) {
     return this.cartService.create(body);
   }
 
-  @Get('carrinhos')
+  @Get()
   @ApiOperation({
     summary: 'Lista todos os carrinhos',
-    description: 'Disponível somente para Administrador'
+    description: 'Disponível somente para Administrador',
   })
   @ApiResponse({ status: 200, description: 'Listagem gerada com sucesso' })
   @ApiResponse({ status: 400, description: 'Erro ao listar carrinhos' })
@@ -36,24 +50,24 @@ export class CartController {
   @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
   @ApiBearerAuth('access_token')
   @CheckPolicies((ability: AppAbility) => ability.can(Action.Admin, 'all'))
-  async findAllUsers(){
-    return this.cartService.findAllUsers()
+  async findAllUsers() {
+    return this.cartService.findAllUsers();
   }
 
   @Get(':id')
-  @ApiOperation({summary: 'Lista um carrinho pelo id'})
+  @ApiOperation({ summary: 'Lista um carrinho pelo id' })
   @ApiResponse({ status: 200, description: 'Listagem gerada com sucesso' })
   @ApiResponse({ status: 401, description: 'Usuário não autorizado' })
   @ApiResponse({ status: 404, description: 'Carrinho não encontrado' })
   @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
   @ApiBearerAuth('access_token')
   @CheckPolicies((ability: AppAbility) => ability.can(Action.User, 'all'))
-  async findById(@Param('id') id: string){
-    return this.cartService.findById(id)
+  async findById(@Param('id') id: string) {
+    return this.cartService.findById(id);
   }
 
   @Get('carrinhos/:userId')
-  @ApiOperation({summary: 'Lista os carrinhos pelo id do usuário'})
+  @ApiOperation({ summary: 'Lista os carrinhos pelo id do usuário' })
   @ApiResponse({ status: 200, description: 'Listagem gerada com sucesso' })
   @ApiResponse({ status: 400, description: 'Erro ao listar carrinhos' })
   @ApiResponse({ status: 401, description: 'Usuário não autorizado' })
@@ -61,12 +75,12 @@ export class CartController {
   @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
   @ApiBearerAuth('access_token')
   @CheckPolicies((ability: AppAbility) => ability.can(Action.User, 'all'))
-  async findAll(@Param('userId') id: string){
+  async findAll(@Param('userId') id: string) {
     return this.cartService.findAll(id);
   }
 
   @Patch(':id')
-  @ApiOperation({summary: 'Atualiza dados de um carrinho'})
+  @ApiOperation({ summary: 'Atualiza dados de um carrinho' })
   @ApiResponse({ status: 200, description: 'Dados atualizados com sucesso' })
   @ApiResponse({ status: 400, description: 'Erro ao atualizar carrinho' })
   @ApiResponse({ status: 401, description: 'Usuário não autorizado' })
@@ -74,12 +88,12 @@ export class CartController {
   @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
   @ApiBearerAuth('access_token')
   @CheckPolicies((ability: AppAbility) => ability.can(Action.User, 'all'))
-  async update(@Param('id') id: string){
+  async update(@Param('id') id: string) {
     return this.cartService.update(id);
   }
 
   @Delete(':id')
-  @ApiOperation({summary: 'Deleta os itens de um carrinho'})
+  @ApiOperation({ summary: 'Deleta os itens de um carrinho' })
   @ApiResponse({ status: 200, description: 'Itens deletados com sucesso' })
   @ApiResponse({ status: 400, description: 'Erro ao deletar carrinho' })
   @ApiResponse({ status: 401, description: 'Usuário não autorizado' })
@@ -87,7 +101,7 @@ export class CartController {
   @ApiResponse({ status: 500, description: 'Erro interno do servidor.' })
   @ApiBearerAuth('access_token')
   @CheckPolicies((ability: AppAbility) => ability.can(Action.User, 'all'))
-  async destroy(@Param('id') id: string){
-    return this.cartService.destroy(id)
+  async destroy(@Param('id') id: string) {
+    return this.cartService.destroy(id);
   }
 }
--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -1,19 +1,22 @@
-import { Body, HttpException, HttpStatus, Injectable, Param } from '@nestjs/common';
+import {
+  Body,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  Param,
+} from '@nestjs/common';
 import { PrismaService } from 'src/database/PrismaService';
 import { CreateCartDTO } from './dto/cart.create.dto';
 
 @Injectable()
 export class CartService {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) {}
 
   async create(@Body() body: CreateCartDTO) {
-    const {
-      userId,
-      products
-    } = body;
+    const { userId, products } = body;
 
     const productsByDatabase = await this.prisma.products.findMany({
-      where: { id: { in: products.map(p => p.productId) } },
+      where: { id: { in: products.map((p) => p.productId) } },
     });
 
     if (productsByDatabase.length === 0) {
@@ -21,14 +24,15 @@ export class CartService {
     }
 
     let total = 0;
-    const productQuantity = productsByDatabase.map(product => {
-      const quantity = products.find(p => p.productId === product.id)?.quantity || 0;
+    const productQuantity = productsByDatabase.map((product) => {
+      const quantity =
+        products.find((p) => p.productId === product.id)?.quantity || 0;
       total += product.price * quantity;
       return { ...product, quantity };
     });
 
-    const sessionId = ''
-    const paymentId = ''
+    const sessionId = '';
+    const paymentId = '';
 
     const cart = await this.prisma.carts.create({
       data: {
@@ -39,17 +43,15 @@ export class CartService {
       },
       select: {
         id: true,
-        sessionId: true,
-        paymentId: true,
         status: true,
         userId: true,
         total: true,
-      }
+      },
     });
-    if(!cart) 
-      throw new HttpException('Erro ao criar carrinho', HttpStatus.BAD_REQUEST)
+    if (!cart)
+      throw new HttpException('Erro ao criar carrinho', HttpStatus.BAD_REQUEST);
 
-    const cartItems = productQuantity.map(product => ({
+    const cartItems = productQuantity.map((product) => ({
       quantity: product.quantity,
       subTotal: product.price * product.quantity,
       cartId: cart.id,
@@ -63,17 +65,35 @@ export class CartService {
     return cart;
   }
 
-  async findById(id: string){
-    const cart = await this.prisma.carts.findUnique({where: {id}})
+  async findById(id: string) {
+    const cart = await this.prisma.carts.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        status: true,
+        total: true,
+        userId: true,
+        CartItems: {
+          select: {
+            id: true,
+            quantity: true,
+            productId: true,
+          },
+        },
+      },
+    });
 
-    if(!cart) throw new HttpException('Carrinho não encontrado', HttpStatus.NOT_FOUND)
+    if (!cart)
+      throw new HttpException('Carrinho não encontrado', HttpStatus.NOT_FOUND);
 
     return cart;
   }
 
-  async findAll(@Param() id: string){
+  async findAll(@Param() id: string) {
     const cart = await this.prisma.carts.findMany({
-      where:{
+      where: {
         userId: id,
       },
       orderBy: {
@@ -86,83 +106,98 @@ export class CartService {
         status: true,
         userId: true,
         total: true,
-        CartItems: true
+        CartItems: true,
       },
     });
 
-    if(!cart) 
-      throw new HttpException('Erro ao listar carrinhos', HttpStatus.BAD_REQUEST)
+    if (!cart)
+      throw new HttpException(
+        'Erro ao listar carrinhos',
+        HttpStatus.BAD_REQUEST,
+      );
 
     return cart;
   }
 
-  async findAllUsers(){
+  async findAllUsers() {
     const carts = await this.prisma.carts.findMany({
       orderBy: {
-        id: 'desc'
+        id: 'desc',
       },
       select: {
         id: true,
-        sessionId: true,
-        paymentId: true,
         status: true,
         userId: true,
         total: true,
-        CartItems: true
-      }
-    })
+        CartItems: true,
+      },
+    });
 
-    if(!carts) 
-      throw new HttpException('Erro ao listar carrinhos', HttpStatus.BAD_REQUEST)
+    if (!carts)
+      throw new HttpException(
+        'Erro ao listar carrinhos',
+        HttpStatus.BAD_REQUEST,
+      );
     return carts;
   }
 
-  async update(id: string){
-    const checkCart = await this.prisma.carts.findUnique({where: {id}})
-    if(!checkCart) 
-      throw new HttpException('Carrinho não encontrado', HttpStatus.NOT_FOUND)
+  async update(id: string) {
+    const checkCart = await this.prisma.carts.findUnique({ where: { id } });
+    if (!checkCart)
+      throw new HttpException('Carrinho não encontrado', HttpStatus.NOT_FOUND);
 
     const updatedCart = await this.prisma.carts.update({
       where: {
-        id
-      }, 
-      data: {
-
-      }
-    })
-    if(!updatedCart)
-      throw new HttpException('Erro ao atualizar carrinho', HttpStatus.BAD_REQUEST)
+        id,
+      },
+      data: {},
+    });
+    if (!updatedCart)
+      throw new HttpException(
+        'Erro ao atualizar carrinho',
+        HttpStatus.BAD_REQUEST,
+      );
 
     return updatedCart;
   }
 
+  async destroy(id: string) {
+    const checkCart = await this.prisma.carts.findUnique({ where: { id } });
+    if (!checkCart)
+      throw new HttpException('Carrinho não encontrado', HttpStatus.NOT_FOUND);
 
-  async destroy(id: string){
-    const checkCart = await this.prisma.carts.findUnique({where: {id}})
-    if(!checkCart) 
-      throw new HttpException('Carrinho não encontrado', HttpStatus.NOT_FOUND)
+    if (checkCart.status == 'PAGO' || checkCart.status == 'CANCELADO')
+      throw new HttpException(
+        'O carrinho não pode ser deletado',
+        HttpStatus.BAD_REQUEST,
+      );
 
-    if(checkCart.status == 'PAGO' || checkCart.status == 'CANCELADO') 
-      throw new HttpException('O carrinho não pode ser deletado', HttpStatus.BAD_REQUEST)
-
-    const deletedItems = await this.prisma.cartItems.deleteMany({where: {cartId: checkCart.id}})
-    if(!deletedItems)
-      throw new HttpException('Erro ao deletar carrinho', HttpStatus.BAD_REQUEST)
+    const deletedItems = await this.prisma.cartItems.deleteMany({
+      where: { cartId: checkCart.id },
+    });
+    if (!deletedItems)
+      throw new HttpException(
+        'Erro ao deletar carrinho',
+        HttpStatus.BAD_REQUEST,
+      );
 
     const updatedCart = await this.prisma.carts.update({
       where: {
-        id
-      }, 
+        id,
+      },
       data: {
-        total: 0
-      }
-    })
-    if(!updatedCart)
-      throw new HttpException('Erro ao atualizar carrinho', HttpStatus.BAD_REQUEST)
+        total: 0,
+      },
+    });
+    if (!updatedCart)
+      throw new HttpException(
+        'Erro ao atualizar carrinho',
+        HttpStatus.BAD_REQUEST,
+      );
 
-    return ({
+    return {
       message: 'Itens deletados com sucesso',
-      cart: updatedCart
-    })
+      cart: updatedCart,
+    };
   }
 }
