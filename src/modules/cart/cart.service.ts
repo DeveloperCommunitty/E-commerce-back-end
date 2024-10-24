9bf2f867bfe5cbfeae4d48d3083a498a9343d093
PHziinn <skamikaze4@gmail.com>
2024-10-24 00:09:04 -0300

    teste

--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -141,7 +141,7 @@ export class CartService {
     return cart;
   }
 
-  @Cron('*/5 * * * *')
+  @Cron('*/30 * * * *')
   async unlockExpiredProducts() {
     const now = new Date();
     console.log(now);
